94f4faeb984802e0abffc8e7fb482acfe293e2e6
cxyzzz <2283361094@qq.com>
2019-06-27 20:51:57 +0800

    判断闰年

---
+++ day_2/1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+  int a;
+  scanf("%d", &a);
+  if(a%4 == 0 && a%100 !=0)
+  {
+    printf("是闰年\n");
+  }
+  else
+  {
+    printf("不是闰年\n");
+  }
+  
+  return 0;
+}
